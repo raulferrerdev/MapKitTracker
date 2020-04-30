70e1f4b148b86574f491ddbff5898b4f350b7a69
raulferrerdev <raul.ferrer.dev@gmail.com>
2020-04-30 08:37:57 +0200

    Show directions.

--- MapTracker/ViewController.swift
+++ MapTracker/ViewController.swift
@@ -13,26 +13,87 @@ class ViewController: UIViewController {
     
     private let mapView = MKMapView(frame: .zero)
     private let locationManager = CLLocationManager()
-    private let rangeInMeters: Double = 2000
-
+    private let rangeInMeters: Double = 500
+    private let mapTypecontroller = UISegmentedControl(items: ["Standard", "Satellite", "Hybrid"])
+    private let pointer = UIImageView(image: UIImage(systemName: "mappin"))
+    private let addressLabel = UILabel(frame: .zero)
+    private let geoCoder = CLGeocoder()
+    
+    private var previousLocation: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        configureAddressLabel()
         layoutUI()
+        configureMapTypeController()
         checkLocationServices()
+    }
+    
+    
+    private func configureMapTypeController() {
+        mapTypecontroller.selectedSegmentIndex = 0
+        mapView.mapType = .standard
+        mapTypecontroller.addTarget(self, action: #selector(indexChanged(_:)), for: .valueChanged)
+    }
+    
+    
+    private func configureAddressLabel() {
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        addressLabel.font = .systemFont(ofSize: 18.0, weight: .medium)
+        addressLabel.textColor = .darkText
+        addressLabel.textAlignment = .center
+        addressLabel.backgroundColor = .init(white: 1, alpha: 0.75)
+        addressLabel.layer.cornerRadius = 5.0
+        addressLabel.clipsToBounds = true
+    }
+    
+    
+    @objc private func indexChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            mapView.mapType = .mutedStandard
+        case 1:
+            mapView.mapType = .satellite
+        case 2:
+            mapView.mapType = .hybrid
+        default:
+            break
+        }
     }
 
     
     private func layoutUI() {
+        mapView.delegate = self
         mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapTypecontroller.translatesAutoresizingMaskIntoConstraints = false
+        pointer.translatesAutoresizingMaskIntoConstraints = false
+        pointer.tintColor = .systemRed
+        
         view.addSubview(mapView)
+        view.addSubview(mapTypecontroller)
+        view.addSubview(pointer)
+        view.addSubview(addressLabel)
         
         NSLayoutConstraint.activate([
             mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            addressLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            addressLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
+            addressLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
+            addressLabel.heightAnchor.constraint(equalToConstant: 40),
+
+            mapTypecontroller.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40),
+            mapTypecontroller.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mapTypecontroller.widthAnchor.constraint(equalToConstant: 300),
+            
+            pointer.centerYAnchor.constraint(equalTo: mapView.centerYAnchor, constant: -14.5),
+            pointer.centerXAnchor.constraint(equalTo: mapView.centerXAnchor),
+            pointer.widthAnchor.constraint(equalToConstant: 27),
+            pointer.heightAnchor.constraint(equalToConstant: 29)
         ])
     }
     
@@ -45,7 +106,6 @@ class ViewController: UIViewController {
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-
         checkAuthorizationForLocation()
     }
     
@@ -56,6 +116,7 @@ class ViewController: UIViewController {
             mapView.showsUserLocation = true
             centerViewOnUser()
             locationManager.startUpdatingLocation()
+            previousLocation = getCenterLocation(for: mapView)
             break
         case .denied:
             // Here we must tell user how to turn on location on device
@@ -78,6 +139,12 @@ class ViewController: UIViewController {
                                                        longitudinalMeters: rangeInMeters)
         mapView.setRegion(coordinateRegion, animated: true)
     }
+    
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let coordinates = mapView.centerCoordinate
+        return CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
 }
 
 
@@ -85,15 +152,36 @@ extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkAuthorizationForLocation()
     }
-    
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let coordinateRegion = MKCoordinateRegion.init(center: location.coordinate,
-                                                       latitudinalMeters: rangeInMeters,
-                                                       longitudinalMeters: rangeInMeters)
+}
+
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
         
-        mapView.setRegion(coordinateRegion, animated: true)
+        guard let previousLocation = self.previousLocation,
+            center.distance(from: previousLocation) > 25 else { return }
+        
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let _ = error { // Show alert for the user
+                return
+            }
+            
+            guard let placemark = placemarks?.first else { // Show alert for the user
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
     }
 }
 
